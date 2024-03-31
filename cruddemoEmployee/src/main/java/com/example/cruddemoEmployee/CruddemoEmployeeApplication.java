13b1ca29fcc4edb872565bab0ff76fdcfd881a94
Yana <yanakirova1@gmail.com>
2024-03-31 10:43:03 +0100

    first commit

---
+++ cruddemoEmployee/src/main/java/com/example/cruddemoEmployee/CruddemoEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.cruddemoEmployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruddemoEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoEmployeeApplication.class, args);
+	}
+
+}
---
+++ cruddemoEmployee/src/test/java/com/example/cruddemoEmployee/CruddemoEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cruddemoEmployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
