6846e930d1f0677ef4f32eaaa2780e59a347f4a3
Yana <yanakirova1@gmail.com>
2024-04-01 12:54:32 +0100

    changing root mapping to api/members

--- cruddemoEmployee/src/main/java/com/example/cruddemoEmployee/dao/EmployeeRepository.java
+++ cruddemoEmployee/src/main/java/com/example/cruddemoEmployee/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.example.cruddemoEmployee.dao;
 
 import com.example.cruddemoEmployee.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
