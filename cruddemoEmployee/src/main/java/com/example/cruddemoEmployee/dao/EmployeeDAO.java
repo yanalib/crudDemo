25f37427a65d78e56968112638ab0a9938fc44b4
Yana <yanakirova1@gmail.com>
2024-04-01 11:34:13 +0100

    use Spring Data JPA instead of Standard JPA, removing DAO interface and implementation

--- cruddemoEmployee/src/main/java/com/example/cruddemoEmployee/dao/EmployeeDAO.java
+++ cruddemoEmployee/src/main/java/com/example/cruddemoEmployee/dao/EmployeeDAO.java
@@ -1,11 +1,20 @@
-package com.example.cruddemoEmployee.dao;
+/*
 
-import com.example.cruddemoEmployee.entity.Employee;
-import java.util.List;
+This interface is not needed as we use Spring Data JPA -> EmployeeRepository
 
-public interface EmployeeDAO {
-    List<Employee> findAll();
-    Employee findById(int theId);
-    Employee save(Employee theEmployee);
-    void deleteById(int id);
-}
+*/
+
+
+
+
+//package com.example.cruddemoEmployee.dao;
+//
+//import com.example.cruddemoEmployee.entity.Employee;
+//import java.util.List;
+//
+//public interface EmployeeDAO {
+//    List<Employee> findAll();
+//    Employee findById(int theId);
+//    Employee save(Employee theEmployee);
+//    void deleteById(int id);
+//}
--- cruddemoEmployee/src/main/java/com/example/cruddemoEmployee/service/EmployeeServiceImpl.java
+++ cruddemoEmployee/src/main/java/com/example/cruddemoEmployee/service/EmployeeServiceImpl.java
@@ -1,40 +1,44 @@
 package com.example.cruddemoEmployee.service;
 
 import com.example.cruddemoEmployee.dao.EmployeeDAO;
+import com.example.cruddemoEmployee.dao.EmployeeRepository;
 import com.example.cruddemoEmployee.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
-    private EmployeeDAO employeeDAO;
+    private EmployeeRepository employeeRepository;
     //inject employee DAO with constructor injection
     @Autowired
-    public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO) {
-        employeeDAO = theEmployeeDAO;
+    public EmployeeServiceImpl( EmployeeRepository theEmployeeRepository) {
+        employeeRepository = theEmployeeRepository;
     }
 
     @Override
-    public List<Employee> findAll() {
-        return employeeDAO.findAll();
-    }
+    public List<Employee> findAll() { return employeeRepository.findAll(); }
 
     @Override
     public Employee findById(int theId) {
-        return employeeDAO.findById(theId);
+        Optional<Employee> result = employeeRepository.findById(theId);
+
+        Employee theEmployee = null;
+
+        if (result.isPresent()) {
+            theEmployee = result.get();
+        }
+        else {
+            throw new RuntimeException("Employee Id not found -" + theId);
+        }
+        return theEmployee;
     }
 
     @Override
-    @Transactional
-    public Employee save(Employee theEmployee) {
-        return employeeDAO.save(theEmployee);
-    }
-
+    public Employee save(Employee theEmployee) { return employeeRepository.save(theEmployee); }
     @Override
-    @Transactional
-    public void deleteById(int id) {
-        employeeDAO.deleteById(id);
-    }
+    public void deleteById(int id) { employeeRepository.deleteById(id); }
 }
