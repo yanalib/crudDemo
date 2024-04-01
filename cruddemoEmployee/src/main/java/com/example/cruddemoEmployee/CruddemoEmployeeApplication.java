3c0dfbe20ce39a1e07d08289eee14558e793053b
Yana <yanakirova1@gmail.com>
2024-04-01 12:32:40 +0100

    make use of Spring Data REST - deleting service layer and rest controller

--- cruddemoEmployee/src/main/java/com/example/cruddemoEmployee/CruddemoEmployeeApplication.java
+++ cruddemoEmployee/src/main/java/com/example/cruddemoEmployee/CruddemoEmployeeApplication.java
@@ -2,6 +2,7 @@ package com.example.cruddemoEmployee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableLoadTimeWeaving;
 
 @SpringBootApplication
 public class CruddemoEmployeeApplication {
