e2cb4ecd8e980fb7e0e6004f9140db0a864ef27d
Yana <yanakirova1@gmail.com>
2024-04-07 15:34:07 +0100

    adding the SQL script for tables needed for JDBC default settings for Spring Security

--- cruddemoEmployee/src/main/java/com/example/cruddemoEmployee/security/SecurityConfig.java
+++ cruddemoEmployee/src/main/java/com/example/cruddemoEmployee/security/SecurityConfig.java
@@ -2,10 +2,14 @@ package com.example.cruddemoEmployee.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -31,5 +35,23 @@ public class SecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, sonya);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer -> configurer
+                        .requestMatchers(HttpMethod.GET, "/api/members").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/members/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/members").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/members/**").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/members/**").hasRole("ADMIN")
+        );
+        //Use Basic Auth
+        http.httpBasic(Customizer.withDefaults());
+
+        //disable CSRF
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
     }
 }
